a2f2199fc6ad3212cfdd17b2bef9cf183e622d99
gabrieldmonteiro <gabrieldmonteiro@live.com>
2017-11-17 16:59:12 -0200

    _

---
+++ app/src/main/java/uam/trabalhocm/DadosPessoaActivity.java
@@ -0,0 +1,13 @@
+package uam.trabalhocm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DadosPessoaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_pessoa);
+    }
+}
---
+++ app/src/main/java/uam/trabalhocm/Usuario.java
@@ -0,0 +1,52 @@
+package uam.trabalhocm;
+
+public class Usuario {
+    private String login;
+    private String senha;
+    private String email;
+    private float altura;
+    private float peso;
+
+    public Usuario() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+}
